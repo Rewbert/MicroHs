5a50e98414f3eb63159e372a6a9dec64c0f7d96f
Rewbert <krookr@chalmers.se>
2025-09-29 08:42:05 +0200

    Some comments

--- lib/Network/Network.hs
+++ lib/Network/Network.hs
@@ -1,3 +1,10 @@
+{-| Simple Socket interface, that only supports AF_INET so far. There are other addressing families (e.g. BT)
+that are not supported (because I am lazy).
+
+Most functions only exist as IO () variants, where in reality they might fail with an error code.
+These variants can be trivially added.
+
+-}
 module Network.Network where
 
 import Data.Char
@@ -24,43 +31,59 @@ foreign import ccall "unistd.h     close"   c_close   :: CInt -> IO CInt
 
 foreign import ccall "errno.h      &errno"  cerrno    :: IO (Ptr CInt)
 
+-- | Socket type
 newtype Socket = Socket CInt
 
-data Domain = AF_INET | INETV6 -- many more
+-- | Adressing family
+data Domain
+    = AF_INET -- ^ IPV4
+    | INETV6  -- ^ IPV6
+    -- many more that are not added
 
 instance Enum Domain where
     toEnum 2 = AF_INET
     toEnum 10 = INETV6
+    toEnum _ = "not added yet"
 
     fromEnum AF_INET = 2
     fromEnum INETV6 = 10
 
-data StreamType = SOCK_STREAM | DGRAM -- many more
+-- | Stream type
+data StreamType
+    = SOCK_STREAM -- ^ TCP
+    | DGRAM       -- ^ UDP
+    -- many more to add
 
 instance Enum StreamType where
     toEnum 1 = SOCK_STREAM
-    toEnum _ = error "idk yet, look it up"
+    toEnum _ = error "not added yet"
 
     fromEnum SOCK_STREAM = 1
-    fromEnum DGRAM = error "idk yet, look it up"
+    fromEnum DGRAM = error "not added yet"
 
+-- | Socket Address type, don't expose internals of this one
 data SockAddr = SockAddrInet Int String
 
+{- | Create a SockAddr. If the second parameter is Nothing, the address is assumed
+to be INADDR_ANY (0.0.0.0) -}
 mkSockAddr :: Int -> Maybe String -> SockAddr
 mkSockAddr port (Just address) = SockAddrInet port address
-mkSockAddr port Nothing        = SockAddrInet port "0.0.0.0"
+mkSockAddr port Nothing        = SockAddrInet port "0.0.0.0" -- INADDR_ANY
 
+-- | There really should be a Storable instance instead, where we can implement the sizeOf function
 sizeOfSockAddr :: Int
 sizeOfSockAddr = 16 -- 8 bytes of data, and 8 of padding
 
-data SockOpt = SO_REUSEADDR -- and many more
+-- | Socket options
+data SockOpt
+    = SO_REUSEADDR -- ^ Allow reuse of local addresses
+    -- and many more to add, I just need this one for my specific application
 
 instance Enum SockOpt where
     toEnum 2 = SO_REUSEADDR
-    toEnum _ = error "idk yet, look it up"
+    toEnum _ = error "not added yet"
 
     fromEnum SO_REUSEADDR = 2
-    fromEnum _ = error "idk yet, look it up"
 
 -- | Create a new socket, throwing an exception if creation failed
 socket :: Domain -> StreamType -> IO Socket
@@ -70,8 +93,11 @@ socket d st = do
         then error "error allocating socket"
         else return $ Socket i
 
+-- | Set a socket option
 setsocketopt :: Socket -> SockOpt -> IO ()
 setsocketopt (Socket socketfd) so = do
+    -- please see the C setsocketopt to see what the parameters are, if it is unclear
+    -- (it was unclear to me first)
     callocaBytes (sizeOf (undefined :: CInt)) $ \p -> do
         poke (castPtr p) (1 :: CInt)
         CInt e <- c_setsockopt socketfd (CInt 1 {- SOL_SOCKET = 1 -}) (CInt $ fromEnum so) p (CInt (sizeOf (undefined :: CInt)))
@@ -80,6 +106,7 @@ setsocketopt (Socket socketfd) so = do
                     error $ "error in setsocketopt, errno is " ++ show c
             else return ()
 
+-- | Close a socket
 close :: Socket -> IO ()
 close (Socket fd@(CInt fd')) = do
     CInt c <- c_close fd
@@ -88,6 +115,7 @@ close (Socket fd@(CInt fd')) = do
                 error $ "error closing socket " ++ show fd' ++ ", errno is " ++ show c
         else return ()
 
+-- | Establish a connection
 connect :: Socket -> SockAddr -> IO ()
 connect (Socket socketfd) sockaddr =
     withSockAddr sockaddr $ \p -> do
@@ -97,12 +125,14 @@ connect (Socket socketfd) sockaddr =
                     error $ "error in connect, errno is " ++ show c
             else return ()
 
+-- | Establish a connection, returning a Bool to indicate whether the action was successful
 connect' :: Socket -> SockAddr -> IO Bool
 connect' (Socket socketfd) sockaddr =
     withSockAddr sockaddr $ \p -> do
         CInt e <- c_connect socketfd p (CInt sizeOfSockAddr)
         return $ e >= 0
 
+-- | Bind a socket to a SockAddr
 bind :: Socket -> SockAddr -> IO ()
 bind (Socket socketfd) sockaddr =
     withSockAddr sockaddr $ \p -> do
@@ -112,6 +142,7 @@ bind (Socket socketfd) sockaddr =
                     error $ "error in bind, errno is " ++ show c
             else return ()
 
+-- | Accept an incoming connection request
 accept :: Socket -> IO (Socket, SockAddr)
 accept (Socket server_fd) = do
     allocaBytes sizeOfSockAddr $ \p ->
@@ -125,6 +156,7 @@ accept (Socket server_fd) = do
                 else do sockaddr <- peekSockAddr p
                         return (Socket (CInt e), sockaddr)
 
+-- | Set a socket to listening mode
 listen :: Socket -> IO ()
 listen (Socket df) = do
     CInt e <- c_listen df (CInt 1)
@@ -133,6 +165,7 @@ listen (Socket df) = do
                 error $ "error in listen, errno is " ++ show c
         else return ()
 
+-- | Send a buffer of data over a socket
 sendBuf :: Socket -> Ptr Word8 -> Int -> IO Int
 sendBuf (Socket socketfd) buf len = do
     CSSize e <- c_send socketfd buf (CSize (fromIntegral len)) (CInt 0)
@@ -141,12 +174,14 @@ sendBuf (Socket socketfd) buf len = do
                 error $ "error in send, errno is " ++ show c
         else return e
 
+-- | Convenience function to transmit a String over a socket
 sendString :: Socket -> String -> IO Int
 sendString socket str =
     let bytes = map (fromIntegral . ord) str :: [Word8]
     in withArray bytes $ \ptr ->
         sendBuf socket ptr (length bytes)
 
+-- | Receive data over a socket
 recvBuf :: Socket -> Ptr Word8 -> Int -> IO Int
 recvBuf (Socket socketfd) buf len = do
     CSSize e <- c_recv socketfd buf (CSize (fromIntegral len)) (CInt 0)
@@ -155,52 +190,22 @@ recvBuf (Socket socketfd) buf len = do
                 error $ "error in recv, errno is " ++ show c
         else return e
 
+-- | Convenience function to receive a string (of a maximum length)
 recvString :: Socket -> Int -> IO String
 recvString sock len = allocaBytes len $ \buf -> do
     n <- recvBuf sock buf len
     bytes <- mapM (\i -> peekByteOff buf i :: IO Word8) [0 .. (n-1)]
     return $ map (chr . fromIntegral) bytes
 
-{-
-
-struct sockaddr_in {
-    sa_family_t     sin_family;     /* AF_INET */
-    in_port_t       sin_port;       /* Port number */
-    struct in_addr  sin_addr;       /* IPv4 address */
-};
-
-struct in_addr {
-    in_addr_t s_addr;
-};
-
-typedef uint32_t in_addr_t;
-typedef uint16_t in_port_t;
-
-
-
-
-// sin_family = AF_INET (2)
-raw[0] = 0x02;
-raw[1] = 0x00;
-
-// sin_port = htons(0x1234) → network byte order
-raw[2] = 0x12;  // high byte
-raw[3] = 0x34;  // low byte
-
-// sin_addr = 192.168.0.1
-raw[4] = 192;
-raw[5] = 168;
-raw[6] = 0;
-raw[7] = 1;
-
--}
-
+{- | Socket functions require a pointer to a SockAddr, and this function takes a SockAddr and
+a such a function, and turns the SockAddr into a 'pointable' value (by writing it to the heap). -}
 withSockAddr :: SockAddr -> (Ptr SockAddr -> IO a) -> IO a
 withSockAddr sockaddr f =
     callocaBytes sizeOfSockAddr $ \p -> do
         pokeSockAddr p sockaddr
         f (castPtr p)
 
+-- | Write a SockAddr to a buffer
 pokeSockAddr :: Ptr Word8 -> SockAddr -> IO ()
 pokeSockAddr p (SockAddrInet port address) = do
     -- write AF_INET = 2 to sin_family
@@ -212,9 +217,11 @@ pokeSockAddr p (SockAddrInet port address) = do
     -- write address to sin_addr
     mapM_ (\(i, b) -> pokeByteOff p i b) (zip [4..] sin_addr)
   where
+    -- hard-coded AF_INET for now
     sin_family :: [Word8]
     sin_family = [0x02, 0x00]
 
+    -- the port in network order
     sin_port :: [Word8]
     sin_port =
         let w16 = fromIntegral port :: Word16
@@ -222,6 +229,7 @@ pokeSockAddr p (SockAddrInet port address) = do
             low = fromIntegral (w16 .&. 0xFF)
         in [high, low]
     
+    -- the address (e.g. "127.0.0.1" -> [127,0,0,1] :: [Word8])
     sin_addr :: [Word8]
     sin_addr = map read $ splitOn '.' address
       where
@@ -233,6 +241,7 @@ pokeSockAddr p (SockAddrInet port address) = do
                             [] -> [pref]
                             (_:xs') -> pref : splitOn i xs'
 
+-- | Read a SockAddr from a buffer
 peekSockAddr :: Ptr SockAddr -> IO SockAddr
 peekSockAddr p =
     let p' :: Ptr Word8
@@ -252,8 +261,7 @@ peekSockAddr p =
             address = intercalate "." $ map show sin_addr
         return $ SockAddrInet port address
 
-
-
+-- | The current Errno
 errno :: IO Int
 errno = do
     p <- cerrno
